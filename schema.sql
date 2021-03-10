fb2c7a2f2cb2f2c0054bc9f3d26fa36176c05a3b
Esteban Mendez <edmendezn@gmail.com>
2021-03-10 13:01:13 -0500

    Added initial inquirer question

--- schema.sql
+++ schema.sql
@@ -7,10 +7,38 @@ USE employee_tracker_db;
 -- Department table
 CREATE TABLE department (
 -- Department id
-id int not null,
+id int not null auto_increment,
 -- Department name
 name varchar(50),
 
-PRIMARY KEY (id)
-)
+primary key(id)
+);
+
+-- Role table
+CREATE TABLE role (
+id int not null auto_increment,
+title varchar(50) not null,
+salary decimal,
+department_id int,
+
+primary key(id),
+FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Employee table
+CREATE TABLE employee (
+id int not null auto_increment,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+role_id int not null,
+manager_id int,
+
+primary key(id)
+
+);
+
+Insert into department (name)
+Values ("Echo"), ("EKG"), ("Holter");
+
+select * from department;
 
---
+++ tracker.js
@@ -0,0 +1,65 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Meg@deth6",
+  port: 3306,
+  // Create database in mysql
+  database: "employee_tracker_db",
+});
+
+connection.connect((err) => {
+  err ? console.error(err) : askFirstQuestion();
+});
+
+function askFirstQuestion() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "SELECT A TASK FROM THE MENU",
+        choices: [
+          "VIEW departments",
+          "VIEW roles",
+          "VIEW employees",
+          "ADD departments",
+          "ADD roles",
+          "ADD employee",
+          "UPDATE employee roles",
+        ],
+        name: "task",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      const { task } = answers;
+      if (task === "VIEW departments") {
+        viewDepartments();
+      } else if (task === "VIEW roles") {
+        viewRoles();
+      } else if (task === "VIEW employees") {
+        viewEmployees();
+      } else if (task === "ADD departments") {
+        addDepartments();
+      } else if (task === "ADD roles") {
+        addRoles();
+      } else if (task === "ADD employee") {
+        addEmployee();
+      } else if (task === "UPDATE employee roles") {
+        updateEmployeeRoles();
+      } else {
+        connection.end();
+        console.log("GOODBYE!");
+      }
+    });
+}
+
+function viewDepartments() {
+  connection.query("select * from department", (err, results) => {
+    if(err) console.error(err);
+    console.log("Should display departments")
+  });
+}
